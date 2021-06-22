4063cd43e77b4862536e22ba6ff2b27eb2cd9e74
Trevor Lemeron <trevorrl@me.com>
2021-06-22 17:13:11 -0400

    more testing

--- test/regular/grow.ts
+++ test/regular/grow.ts
@@ -1,38 +1,70 @@
 import { test, useSpectron  } from "../helpers/spectron";
 import { logIn } from '../helpers/spectron/user';
-import { FormMonkey } from '../helpers/form-monkey';
 import { sleep } from "../helpers/sleep";
 
 useSpectron();
 
-const GOAL_TITLE = 'My Custom Goal';
-const GOAL_TOTAL = '5';
+const BAD_GOAL_TITLE = '123456789012345678901234567890123';
+const BAD_GOAL_TOTAL = '\uE003\uE003100';
 
-test('Goals Section', async t => {
+const GOOD_GOAL_TITLE = 'Automated Custom Goal';
+const GOOD_GOAL_TOTAL = '\uE003\uE00310';
+
+test('Each Section Loads Properly', async t => {
     const app = t.context.app;
-    const formMonkey = new FormMonkey(t);
 
     await logIn(t);
     await (await app.client.$('div[title=Grow]')).click();
 
-    // t.true(await (await app.client.$('h2=My Goals')).isExisting());
-
-    await (await app.client.$('span=Add Goal')).click();
-
-    await sleep(10000);
-
-    await (await app.client.$('[data-title="Goal Title"] input')).setValue(GOAL_TITLE);
-
-    await sleep(10000);
-
-    await (await app.client.$('[data-title="Goal Total"] input')).setValue(GOAL_TOTAL);
-
-    await sleep(10000);
-
-    // await formMonkey.setInputValue('Goal Title', GOAL_TITLE);
-    // await formMonkey.setInputValue('Goal Total', GOAL_TOTAL);
-
-    // await (await app.client.$('button=OK')).click();
-
-    // t.true(await (await app.client.$('strong=My Custom Goal')).isExisting());
+    // Check for My Goals section
+    t.true(await (await app.client.$('h2=My Goals')).isExisting());
+    // Check for Growth Tips section
+    t.true(await (await app.client.$('h2=Growth Tips')).isExisting());
+    // Check for Community Reach section
+    t.true(await (await app.client.$('span=Twitch')).isExisting());
+    // Check for Stream Pulse section
+    t.true(await (await app.client.$('h2=Stream Pulse')).isExisting());
+    // Check for Streamer Resources section
+    t.true(await (await app.client.$('h2=Streamer Resources')).isExisting());
 });
+
+test('Add Goal Field Validation Works', async t => {
+    const app = t.context.app;
+
+    await logIn(t);
+    await (await app.client.$('div[title=Grow]')).click();
+
+    // Have to click twice due to some weirdness w/ AntD buttons/modals
+    await (await app.client.$('span=Add Goal')).click();
+    await (await app.client.$('.ant-modal-wrap')).click();
+
+    // Set title and total to invalid values
+    await (await app.client.$('[data-title="Goal Title"]')).setValue(BAD_GOAL_TITLE);
+    await (await app.client.$('[data-title="Goal Total"]')).setValue(BAD_GOAL_TOTAL);
+
+    // Check Title validation
+    t.true(await (await app.client.$('div=Goal Title cannot be more than 32 characters')).isExisting());
+
+    // Check Total validation
+    t.true(await (await app.client.$('div=Goal Total must be between 1 and 50')).isExisting());
+});
+
+test('Add a Custom Goal', async t => {
+    const app = t.context.app;
+
+    await logIn(t);
+    await (await app.client.$('div[title=Grow]')).click();
+
+    // Have to click twice due to some weirdness w/ AntD buttons/modals
+    await (await app.client.$('span=Add Goal')).click();
+    await (await app.client.$('.ant-modal-wrap')).click();
+
+    // Set title and total to valid values
+    await (await app.client.$('[data-title="Goal Title"]')).setValue(GOOD_GOAL_TITLE);
+    await (await app.client.$('[data-title="Goal Total"]')).setValue(GOOD_GOAL_TOTAL);
+
+    await (await app.client.$('button=OK')).click();
+    await (await app.client.$('.ant-modal-wrap')).click();
+
+    await sleep(100000);
+})
